e3b35edf66aadd99dbb057857e2dd8a76eddff03
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-16 15:24:42 +0100

    wrote checkcodeword programme added spec

---
+++ lib/check_codeword.rb
@@ -0,0 +1,9 @@
+def check_codeword(codeword)
+  if codeword == "horse"
+    return "Correct! Come in."
+  elsif code.chars.first == "h"
+    return "Close, but nope."
+  else
+    return "WRONG!"
+  end
+end
