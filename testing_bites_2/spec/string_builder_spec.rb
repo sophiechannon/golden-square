f6698054e325fb5e38084cd6517d47729479cd79
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-17 11:09:50 +0100

    edited string builder to utilise context feature

--- testing_bites_2/spec/string_builder_spec.rb
+++ testing_bites_2/spec/string_builder_spec.rb
@@ -1,26 +1,28 @@
 require "string_builder"
 
 RSpec.describe StringBuilder do
-  it "adds a string and returns it" do
-    string = StringBuilder.new
-    string.add("yo")
-    expect(string.output).to eq "yo"
+  context "given a string" do
+    it "adds a string and returns it" do
+      string = StringBuilder.new
+      string.add("yo")
+      expect(string.output).to eq "yo"
+    end
+    it "returns the length of a string" do
+      string = StringBuilder.new
+      string.add("yo")
+      expect(string.size).to eq 2
+    end
   end
 
-  it "returns an empty string if no string is added first" do
-    string = StringBuilder.new
-    expect(string.output).to eq ""
-  end
-
-  it "returns the length of a string" do
-    string = StringBuilder.new
-    string.add("yo")
-    expect(string.size).to eq 2
-  end
-
-  it "returns the length of a string even if no string added" do
-    string = StringBuilder.new
-    expect(string.size).to eq 0
+  context "not given a string" do
+    it "returns an empty string" do
+      string = StringBuilder.new
+      expect(string.output).to eq ""
+    end
+    it "returns 0 for length" do
+      string = StringBuilder.new
+      expect(string.size).to eq 0
+    end
   end
 
 end
