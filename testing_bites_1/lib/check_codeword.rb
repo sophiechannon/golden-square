66ea5baedfbfec2964f5f140423f7c63a0ff85ac
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-17 12:30:11 +0100

    added part of code to codeword checker than I missed first time round

--- testing_bites_1/lib/check_codeword.rb
+++ testing_bites_1/lib/check_codeword.rb
@@ -1,7 +1,7 @@
 def check_codeword(codeword)
   if codeword == "horse"
     return "Correct! Come in."
-  elsif codeword.chars.first == "h"
+  elsif codeword.chars.first == "h" && codeword.chars.last == "e"
     return "Close, but nope."
   else
     return "WRONG!"
--- testing_bites_1/spec/check_codeword_spec.rb
+++ testing_bites_1/spec/check_codeword_spec.rb
@@ -5,11 +5,16 @@ RSpec.describe "check_codeword method" do
     expect(check_codeword("horse")).to eq "Correct! Come in."
   end
 
-  it "returns close message if codeword begins with h" do
+  it "returns close message if codeword begins with h and ends with e" do
     expect(check_codeword("house")).to eq "Close, but nope."
   end
 
-  it "returns NOPE message if codeword is incorrect and doesn't begin with h" do
+  it "returns NOPE message if codeword is incorrect and doesn't meet criterior for close" do
     expect(check_codeword("show")).to eq "WRONG!"
+  end
+
+  it "returns NOPE message if one element of close is met, but not both" do
+    expect(check_codeword("hippo")).to eq "WRONG!"
+    expect(check_codeword("sophie")).to eq "WRONG!"
   end
 end
