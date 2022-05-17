52928815fb60f5a2bc12e9383fef3d5a5ee7c70d
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-17 11:56:03 +0100

    tests complete for present

---
+++ testing_bites_3/spec/present_spec.rb
@@ -0,0 +1,24 @@
+require "present"
+
+# if present is wrapped the unwrapped, then it returns the contents (passes)
+# if present is unwrapped before it's wrapped then it throws error
+# if present is wrapped twice in a row it throws an error
+
+RSpec.describe Present do
+  it "returns the contents" do
+    present = Present.new
+    expect(present.wrap(["doll", "socks", "chocolate"])).to eq ["doll", "socks", "chocolate"]
+  end
+
+  it "throws an error if you try to unwrap before wrapping" do
+    present = Present.new
+    expect{ present.unwrap }.to raise_error "No contents have been wrapped."
+  end
+
+  it "throws an error if you try to wrap when present is already wrapped" do
+    present = Present.new
+    present.wrap(["doll", "socks", "chocolate"])
+    expect{ present.wrap(["puppy", "coal"]) }.to raise_error "Contents has already been wrapped."
+  end
+
+end
