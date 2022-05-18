77b9e64bddaac89ae9189618c8d2dde23c0fa2aa
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-18 21:36:05 +0100

    tidying up

--- test-drive-classes/lib/diary.rb
+++ test-drive-classes/lib/diary.rb
@@ -6,19 +6,14 @@ class DiaryEntry
   end
 
   def title
-    if @title == ""
-      fail "Enter Title"
-    else
-      @title
-    end
+    fail "Enter Title" if @title == ""
+    @title
   end
 
   def contents
-    if @contents == ""
+    fail "Enter Content" if @contents == ""
       fail "Enter Content"
-    else
       @contents
-    end
   end
 
   def count_words
@@ -27,33 +22,23 @@ class DiaryEntry
 
 
   def reading_time(wpm)
-    if wpm == 0
-      fail "Enter a bigger number"
-    else
-      (count_words.to_f / wpm).ceil
-  
-    end
-  
+    fail "Enter a bigger number" if wpm == 0
+    (count_words.to_f / wpm).ceil
   end
 
   def reading_chunk(wpm, minutes)
     amount_we_can_read = wpm * minutes
     start_at = @words_read
     end_at = @words_read + amount_we_can_read
-
     words_to_show = @contents.split(" ")[start_at...end_at]
     if words.length == @words_read + words_to_show.length
       @words_read = 0
     else
       @words_read = amount_we_can_read
     end 
-     
-
-
     words_to_show.join(" ")
   end
-
-
+  
   private
 
   def words
