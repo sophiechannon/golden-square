db732e451baaad893220abab3c6fda284b17a5ad
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-18 11:51:53 +0100

    cipher update

--- debugging/lib/encode_decode.rb
+++ debugging/lib/encode_decode.rb
@@ -1,6 +1,7 @@
 def encode(plaintext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
-  cipher
+  # p ('a'...'z').to_a not going all the way to z 
+  p cipher
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char)).chr
   end
@@ -9,8 +10,10 @@ end
 
 def decode(ciphertext, key)
   cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  p cipher
   plaintext_chars = ciphertext.chars.map do |char|
-    cipher[char.ord - 65]
+    cipher[char.ord - 65] 
+    # was returning nil because this created a -venumber when it was 65 - char.ord. Needed reversing.
   end
   return plaintext_chars.join
 end
