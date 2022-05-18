7e4462462bceb4263e91ef8f17c6dbc363917a14
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-18 16:53:41 +0100

    added some notes about what else we should test for

--- test-drive-classes/spec/diary_spec.rb
+++ test-drive-classes/spec/diary_spec.rb
@@ -12,12 +12,18 @@ RSpec.describe DiaryEntry do
     expect(diary_entry.count_words).to eq 2
   end
 
-  it "tells you how long it will take to read in minutes, rounded up" do
-    diary_entry = DiaryEntry.new("Sophie's Diary", "My contents")
-    expect(diary_entry.reading_time(200)).to eq 1
+  context "when given a number greated than 0 as wpm" do
+    it "tells you how long it will take to read in minutes, rounded up" do
+      diary_entry = DiaryEntry.new("Sophie's Diary", "My contents")
+      expect(diary_entry.reading_time(200)).to eq 1
+    end
   end
 
-  # check reading_time fails if reading time == 0
+  context "when given 0 as wpm" do
+    it "fails" do
+      # write a test for this
+    end
+  end
 
   it "Returns a string with a chunk of the contents that the user could read in the given number of minutes." do
     diary_entry = DiaryEntry.new("Sophie's Diary", "a b c d e f g h i j k l m n o p q r s t u v w x y z")
@@ -30,11 +36,15 @@ RSpec.describe DiaryEntry do
     expect(diary_entry.reading_chunk(5, 1)).to eq "f g h i j"
   end
 
-  it "goes back to the beginning" do
-    diary_entry = DiaryEntry.new("Sophie's Diary", "a b c d e f g h i j")
-    expect(diary_entry.reading_chunk(5, 1)).to eq "a b c d e"
-    expect(diary_entry.reading_chunk(5, 1)).to eq "f g h i j"
-    expect(diary_entry.reading_chunk(5, 1)).to eq "a b c d e"
+  context "reading_chunks loops back to the beginning" do
+      it "works if word_count is divisible by wpm * minutes" do
+        diary_entry = DiaryEntry.new("Sophie's Diary", "a b c d e f g h i j")
+        expect(diary_entry.reading_chunk(5, 1)).to eq "a b c d e"
+        expect(diary_entry.reading_chunk(5, 1)).to eq "f g h i j"
+        expect(diary_entry.reading_chunk(5, 1)).to eq "a b c d e"
+    end
+    it "works if word_count is divisible by wpm * minutes" do
+    end
   end
 
   # think about different examples i.e. text already fits within time
