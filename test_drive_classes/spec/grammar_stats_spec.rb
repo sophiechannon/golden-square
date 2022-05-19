989564637a5f35cbd012c9a321f6de2880970b7e
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-19 12:01:55 +0100

    more specifiic method descriptions

--- test_drive_classes/spec/grammar_stats_spec.rb
+++ test_drive_classes/spec/grammar_stats_spec.rb
@@ -5,19 +5,20 @@ RSpec.describe GrammarStats do
     grammar_stats = GrammarStats.new
     expect { grammar_stats.check("")}.to raise_error "No text was provided!"
   end
+  describe "percentage_good method" do
+    it "percentage_good method returns correct perccentage" do
+      grammar_stats = GrammarStats.new
+      grammar_stats.check("hello")
+      grammar_stats.check("hello.")
+      grammar_stats.check("I'm not valid")
+      grammar_stats.check("I'm all good!") # passing text
+      expect(grammar_stats.percentage_good).to eq 25
+    end
 
-  it "percentage_good method returns correct perccentage" do
-    grammar_stats = GrammarStats.new
-    grammar_stats.check("hello")
-    grammar_stats.check("hello.")
-    grammar_stats.check("I'm not valid")
-    grammar_stats.check("I'm all good!") # passing text
-    expect(grammar_stats.percentage_good).to eq 25
-  end
-
-  it "percentage_good method throws error if no checks have been made" do
-    grammar_stats = GrammarStats.new
-    expect { grammar_stats.percentage_good}.to raise_error "No checks have been made yet!"
+    it "percentage_good method throws error if no checks have been made" do
+      grammar_stats = GrammarStats.new
+      expect { grammar_stats.percentage_good}.to raise_error "No checks have been made yet!"
+    end
   end
 end
 
