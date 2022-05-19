b7511d5292c0b9148d1055babf8d347a858ef68c
Dave Kempsell <dave_kempsell@hotmail.com>
2022-05-19 17:29:12 +0100

    exercise complete

--- design_classes/lib/exercise.rb
+++ design_classes/lib/exercise.rb
@@ -4,6 +4,25 @@ class TodoTracker
   end
 
   def add_task(task)
-    fail "Please enter a task." if task == ""
+    if task == ""
+      fail "Please enter a task."
+    elsif @list_of_tasks.include?(task)
+      fail "Task already on list"
+    end
+    @list_of_tasks << task
+  end
+
+  def display_list
+    if @list_of_tasks.empty?
+      return "Nothing to do!"
+    else
+      return @list_of_tasks
+    end
+  end
+
+  def complete_task(task)
+    fail "Task not on list" if !@list_of_tasks.include?(task)
+    @list_of_tasks.delete(task)
+    return @list_of_tasks
   end
 end
--- design_classes/spec/exercise_spec.rb
+++ design_classes/spec/exercise_spec.rb
@@ -7,9 +7,38 @@ RSpec.describe TodoTracker do
       expect {todo.add_task("")}.to raise_error "Please enter a task."
     end
 
-    it "returns #list_of_tasks as array if valid string is entered" do
+    it "fails if task given is already on list" do
       todo = TodoTracker.new
-      expect(todo.add_task("Walk the dog")).to eq ["Walk the dog"]
+      todo.add_task("Walk the dog.")
+      expect {todo.add_task("Walk the dog.")}.to raise_error "Task already on list"
+    end
+  end
+
+  describe "#display_list method" do
+    it "displays #list_of_tasks as an array" do
+      todo = TodoTracker.new
+      todo.add_task("Walk the dog")
+      todo.add_task("Clean the floor")
+      expect(todo.display_list).to eq ["Walk the dog", "Clean the floor"]
+    end
+
+    it "displays 'Nothing to do!' if list is empty" do
+      todo = TodoTracker.new
+      expect(todo.display_list).to eq "Nothing to do!"
+    end
+  end
+
+  describe "#complete_task method" do
+    it "removes task from list when completed" do
+      todo = TodoTracker.new
+      todo.add_task("Walk the dog")
+      todo.add_task("Clean the floor")
+      expect(todo.complete_task("Clean the floor")).to eq ["Walk the dog"]
+    end
+
+    it "fails if task isn't on list" do
+      todo = TodoTracker.new
+      expect {todo.complete_task("Clean the floor")}.to raise_error "Task not on list"
     end
   end
 end
