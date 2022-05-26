771642dd63bf793d1f9f2dada71ccf2251c2506d
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-05-26 16:00:09 +0100

    played around with getting tests to work for confirmation_text unit tests

--- takeaway/spec/confirmation_text_spec.rb
+++ takeaway/spec/confirmation_text_spec.rb
@@ -4,19 +4,19 @@ require "json"
 RSpec.describe ConfirmationText do
   it "constructs" do
     customer = double :customer, name: "Sophie", mobile_number: "+447557942369"
-    requester = double :requester
-    terminal = double :terminal
     text = ConfirmationText.new(customer)
     expect(text.customer.name).to eq "Sophie"
   end
 
-  # I can't get this test to work with this API
-  # xit "sends a message" do
-  #   customer = double :customer, name: "Sophie", mobile_number: "+447557942369"
-  #   requester = double :requester
-  #   text = ConfirmationText.new(customer)
-  #   expect(text.message).to receive(:gets).with("https://api.twilio.com/2010-04-01/Accounts/ACee81d43509a2a51b841d04f24296df35/Messages.json")
-  #   .and_return('{"sid": "SM2767b0752e8e408f8a77efcd1fc2c4a3", "date_created": "Thu, 26 May 2022 11:34:19 +0000", "date_updated": "Thu, 26 May 2022 11:34:19 +0000", "date_sent": null, "account_sid": "ACee81d43509a2a51b841d04f24296df35", "to": "+447557942369", "from": null, "messaging_service_sid": "MGc693f037b4cd513443a7e1e7c04e34da", "body": "testing123", "status": "accepted", "num_segments": "0", "num_media": "0", "direction": "outbound-api", "api_version": "2010-04-01", "price": null, "price_unit": null, "error_code": null, "error_message": null, "uri": "/2010-04-01/Accounts/ACee81d43509a2a51b841d04f24296df35/Messages/SM2767b0752e8e408f8a77efcd1fc2c4a3.json", "subresource_uris": {"media": "/2010-04-01/Accounts/ACee81d43509a2a51b841d04f24296df35/Messages/SM2767b0752e8e408f8a77efcd1fc2c4a3/Media.json"}}')
-  #   expect(text.send).to eq "SM2767b0752e8e408f8a77efcd1fc2c4a3"
-  # end
+  # this works if I have a requester argument for my CnfirmationText class, but then the integration test won't work
+  xit "sends a message" do
+    customer = double :customer, name: "Sophie", mobile_number: "+447557942369"
+    create = double :create, sid: "SM2767b0752e8e408f8a77efcd1fc2c4a3"
+    messages = double :messages, create: create
+    requester = double :requester, messages: messages
+    message = double :message, sid: "SM2767b0752e8e408f8a77efcd1fc2c4a3" 
+    text = ConfirmationText.new(customer, requester)
+    expect(text.send).to eq "SM2767b0752e8e408f8a77efcd1fc2c4a3"
+  end
+
 end
